103371b32dffe5e663c3104df29d2d57d05308fe
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-08-07 17:16:12 -0700

    Passing feature and model tests for owner

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,4 +2,22 @@ class OwnersController < ApplicationController
   def new
     @owner = Owner.new
   end
+
+  def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      redirect_to @owner, notice: "Sucessfully created owner"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @owner = Owner.find(params[:id])
+  end
+  private
+
+  def owner_params
+    params.require(:owner).permit(:first_name, :last_name, :email)
+  end
 end
